759371185cbf8d85b7d2d618459279e24120bd45
Jason Ross <jaross@redhat.com>
2024-11-13 12:50:38 -0600

    chore(strip): strip exposed data

---
+++ mtv_parser/models/migration.py
@@ -0,0 +1,23 @@
+from typing import Self
+
+from pydantic import Field
+
+from .base import K8SRef, ParserBaseModel
+from .status import StatusCondition
+from .timed import TimedBaseModel
+from .vms import VMStatus
+
+
+class MigrationHistory(TimedBaseModel):
+    conditions: list[StatusCondition]
+    migration: K8SRef
+    plan: K8SRef
+
+
+class MigrationStatus(TimedBaseModel):
+    history: list[MigrationHistory] = Field(default_factory=list)
+    vms: list[VMStatus] = Field(default_factory=list)
+
+    @property
+    def succeeded(self: Self) -> bool:
+        return all([vm.succeeded for vm in self.vms])
---
+++ mtv_parser/models/vms.py
@@ -0,0 +1,62 @@
+from enum import StrEnum
+from typing import Self
+
+from pydantic import Field
+
+from .base import K8SRef, ParserBaseModel
+from .status import StatusCondition, StatusConditionType
+from .timed import TimedBaseModel
+
+
+class Phase(StrEnum):
+    COMPLETED = "Completed"
+    PENDING = "Pending"
+
+
+class Unit(StrEnum):
+    MB = "MB"
+
+
+class PipelineProgress(ParserBaseModel):
+    completed: int
+    total: int
+
+
+class PipelineAnnotation(ParserBaseModel):
+    unit: Unit
+
+
+class PipelineName(StrEnum):
+    INITIALIZE = "Initialize"
+    DISKALLOCATION = "DiskAllocation"
+    IMAGECONVERSION = "ImageConversion"
+    DISKTRANSFERV2V = "DiskTransferV2v"
+    VIRTUALMACHINECREATION = "VirtualMachineCreation"
+
+
+class PipelineTask(ParserBaseModel):
+    name: str
+    annotations: PipelineAnnotation | None = None
+    progress: PipelineProgress
+
+
+class PipelineStatus(TimedBaseModel):
+    name: PipelineName
+    phase: Phase
+    tasks: list[PipelineTask] = Field(default_factory=list)
+    annotations: PipelineAnnotation | None = None
+    progess: PipelineProgress
+
+
+class VMStatus(TimedBaseModel):
+    conditions: list[StatusCondition]
+    id: str
+    name: str
+    phase: Phase
+
+    @property
+    def succeeded(self: Self) -> bool:
+        for condition in self.conditions:
+            if condition.type_ == StatusConditionType.SUCCEEDED and condition.status:
+                return True
+        return False
