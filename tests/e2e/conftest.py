949dea5c01bd2deafb6b3940feb8ded01518aab1
Jason Ross <jaross@redhat.com>
2024-11-14 13:11:42 -0600

    test(doc): add docstrings to fixtures since they are non-standard

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -13,6 +13,17 @@ def runner() -> CliRunner:
 
 @pytest.fixture
 def env(request: pytest.FixtureRequest, monkeypatch: pytest.MonkeyPatch) -> Generator[None, Any, None]:
+    """Monkeypatch environment with keys and values from parametrization.
+
+    parameter is expected to be dict[str, str]
+
+    Args:
+        request (pytest.FixtureRequest): pytest request fixture
+        monkeypatch (pytest.MonkeyPatch): pytest monkeypatch fixture
+
+    Yields:
+        None: Uses yield to ensure cleanup runs.
+    """
     env: dict[str, str] = request.param
     for name, value in env.items():
         monkeypatch.setenv(name, value)
@@ -23,8 +34,20 @@ def env(request: pytest.FixtureRequest, monkeypatch: pytest.MonkeyPatch) -> Gene
 
 @pytest.fixture
 def root_command(env: None) -> Generator[BaseCommand, None, None]:
+    """Load CLI module and return root group.
+
+    Imports cli module at test execution time to allow for proper loading of environment variables.
+
+    Args:
+        env (None): Not actually used, but forces pytest to execute env fixture before this one.
+
+    Yields:
+        BaseCommand object, loaded after environment monkeypatching
+    """
     from mtv_parser.cli import root as cmd
 
     yield cmd
-    del sys.modules["mtv_parser.cli"]
-    del sys.modules["cmd"]
+    if "mtv_parser.cli" in sys.modules:
+        del sys.modules["mtv_parser.cli"]
+    if "cmd" in sys.modules:
+        del sys.modules["cmd"]
